90ff1c350d20cae25e25241641bae8c03abe0f6e
philip <philip.k@cottagelabs.com>
2022-06-17 09:23:41 +0100

    set default url

--- siteedit2/urls.py
+++ siteedit2/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/login/login_page')),
     path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
