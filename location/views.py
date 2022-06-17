fb6ff0783f4cb9d9b7f9a8db7c112690d4f4d7a4
philip <philip.k@cottagelabs.com>
2022-06-17 10:43:34 +0100

    update urls settings

--- location/views.py
+++ location/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def form(request):
+    return HttpResponse("It is location form page.")
