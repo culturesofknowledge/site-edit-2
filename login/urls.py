b6264c19e0c13fc4c65de5797c3e8f4ad5ac8552
philip <philip.k@cottagelabs.com>
2022-06-17 10:43:34 +0100

    update urls settings

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from .views import EmloLoginView
 
+app_name = 'login'
 urlpatterns = [
     path('dashboard', views.example_dashboard, name='example dashboard'),
     path('login_page', EmloLoginView.as_view()),
