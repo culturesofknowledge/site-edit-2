281ff1aadc4926518a43a11a7a3e9b8438b37962
philip <philip.k@cottagelabs.com>
2022-06-21 13:45:30 +0100

    implement basic location form

--- location/forms.py
+++ location/forms.py
@@ -1,18 +1,18 @@
-from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, HiddenInput, IntegerField
 
 from location.models import CofkCollectLocation
 
 
 class LocationForm(ModelForm):
-
     # location_name = forms.CharField(label='Location name', max_length=100)
+    location_id = IntegerField()
+    location_id.widget = HiddenInput()
+
     class Meta:
         model = CofkCollectLocation
-        fields = ('location_name',
+        fields = ('location_id',  'location_name',
                   'element_1_eg_room', 'element_2_eg_building',
                   'element_3_eg_parish', 'element_4_eg_city', 'element_5_eg_county',
                   'element_6_eg_country', 'element_7_eg_empire', 'notes_on_place',
                   'editors_notes', 'upload_name', 'location_synonyms',
-                  'latitude', 'longitude', )
-
+                  'latitude', 'longitude',)
--- location/views.py
+++ location/views.py
@@ -1,20 +1,34 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import logging
+
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from location.forms import LocationForm
+from location.models import CofkCollectLocation
+
+log = logging.getLogger(__name__)
 
 
-def form(request):
-    return HttpResponse("It is location form page.")
+def form(request, location_id=None):
+    loc = None
+    location_id = location_id or request.POST.get('location_id')
+    if location_id:
+        loc = get_object_or_404(CofkCollectLocation, pk=location_id)
 
-
-def get_location(request):
-    loc_form = LocationForm(request.POST or None)
+    loc_form = LocationForm(request.POST or None, instance=loc)
     if request.method == 'POST':
         if loc_form.is_valid():
-            loc_form.save()
-            # KTODO do something e.g. validate and save
-            return HttpResponse("Ha Ha Ha Ha....")
+            if loc_form.has_changed():
+                log.info(f'location [{location_id}] have been saved')
+                loc_form.save()
+            else:
+                log.debug('form have no change, skip record save')
+            return redirect('location:search')
 
-    return render(request, 'location/get_location.html', {'form': loc_form})
+    return render(request, 'location/main_form.html', {'form': loc_form, })
+
+
+def search(request):
+    locations = CofkCollectLocation.objects.iterator()
+    return render(request, 'location/search.html',
+                  {'locations': locations})
