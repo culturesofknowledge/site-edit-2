27e97478f6c2c226547383c8345edc46a59272bd
philip <philip.k@cottagelabs.com>
2022-06-24 10:47:22 +0100

    fix admin page for user

--- login/admin.py
+++ login/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CofkUser
 
-admin.site.register(CofkUser)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CofkUser
+    list_display = ['username', 'surname', 'email', 'is_active', 'failed_logins', 'last_login']
+    search_fields = ['username', 'surname', 'forename', 'email', ]
+
+    _personal_fieldsets = ("Personal info", {"fields": ('surname', 'forename', 'email',)})
+    _permission_fieldsets = ("Permission", {
+        'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions',)
+    })
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        _personal_fieldsets,
+        _permission_fieldsets,
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2",
+                           ),
+            },
+        ),
+        _personal_fieldsets,
+        _permission_fieldsets,
+    )
+
+
+admin.site.register(CofkUser, CustomUserAdmin)
