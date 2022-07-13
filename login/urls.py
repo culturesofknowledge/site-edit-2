3407c182f7ccb39fb76931bf7e7038caf705b2d3
philip <philip.k@cottagelabs.com>
2022-07-13 13:41:12 +0100

    fix some url

--- login/urls.py
+++ login/urls.py
@@ -7,4 +7,5 @@ app_name = 'login'
 urlpatterns = [
     path('dashboard', views.example_dashboard, name='example dashboard'),
     path('gate', EmloLoginView.as_view(), name='gate'),
+    path('logout', views.logout_then_login, name='logout'),
 ]
