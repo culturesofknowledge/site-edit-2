d386858019764240f92e1f39f7ea8298be8e9e88
philip <philip.k@cottagelabs.com>
2022-07-19 11:30:42 +0100

    add test cases for search page

--- location/fixtures.py
+++ location/fixtures.py
@@ -14,6 +14,20 @@ location_dict_a = dict(
     latitude='latitude value',
     longitude='longitude value',
 )
+location_dict_b = dict(
+    location_name='location_name value 2',
+    element_1_eg_room='element_1_eg_room value 2',
+    element_2_eg_building='element_2_eg_building value 2',
+    element_3_eg_parish='element_3_eg_parish value 2',
+    element_4_eg_city='element_4_eg_city value 2',
+    element_5_eg_county='element_5_eg_county value 2',
+    element_6_eg_country='element_6_eg_country value 2',
+    element_7_eg_empire='element_7_eg_empire value 2',
+    editors_notes='editors_notes value 2',
+    location_synonyms='location_synonyms value 2',
+    latitude='latitude value 2',
+    longitude='longitude value 2',
+)
 
 loc_res_dict_a = {
     'resource_name': 'resource_name a',
