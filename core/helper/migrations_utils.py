46c4b6c2d01cab2deea0bc285197a63f01182b1c
philip <philip.k@cottagelabs.com>
2022-09-01 12:32:45 +0100

    add default value for change_user, change_timestamp

---
+++ core/helper/migrations_utils.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+from django.db.migrations.operations.base import Operation
+
+
+def create_operation_default_value(table_name, col_name,
+                                   default_val="'__unknown_user'") -> Operation:
+    return migrations.RunSQL(
+        (f"alter table {table_name} "
+         f" alter column {col_name} "
+         f" set default {default_val} "),
+
+        (f'alter table {table_name} '
+         f' alter column {col_name} '
+         ' drop default '),
+    )
+
+
+def create_default_change_user_timestamp(table_name) -> list[Operation]:
+    return [
+        create_operation_default_value(table_name, 'change_user',
+                                       default_val="'__unknown_user'"),
+        create_operation_default_value(table_name, 'change_timestamp',
+                                       default_val="now()"),
+    ]
+
+
+def create_default_empty(table_name, col_names: list[str]) -> list[Operation]:
+    return [
+        create_operation_default_value(table_name, col_name, default_val="''")
+        for col_name in col_names
+    ]
