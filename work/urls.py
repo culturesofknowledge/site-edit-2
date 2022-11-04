2102dc9a8629831f748ef2d15dc30626e667d31b
philip <philip.k@cottagelabs.com>
2022-11-04 11:35:30 +0000

    change work urls style

--- work/urls.py
+++ work/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from core.helper import url_utils
 from login.views import example_dashboard
@@ -6,14 +6,14 @@ from . import views
 
 app_name = 'work'
 urlpatterns = [
-    path(f'form/<int:iwork_id>/corr', views.CorrView.as_view(), name='corr_form'),
-    path(f'form/<int:iwork_id>/dates', views.DatesView.as_view(), name='dates_form'),
-    path(f'form/<int:iwork_id>/places', views.PlacesView.as_view(), name='places_form'),
-    path(f'form/<int:iwork_id>/manif', views.ManifView.as_view(), name='manif_init'),
-    path(f'form/<int:iwork_id>/manif/<str:manif_id>', views.ManifView.as_view(), name='manif_update'),
-    path(f'form/<int:iwork_id>/resources', views.ResourcesView.as_view(), name='resources_form'),
-    path(f'form/<int:iwork_id>/details', views.DetailsView.as_view(), name='details_form'),
-    path(f'form/<int:iwork_id>/overview', views.overview_view, name='overview_form'),
+    re_path(r'form/corr/(<int:iwork_id>)?', views.CorrView.as_view(), name='corr_form'),
+    re_path(r'form/dates/(<int:iwork_id>)?', views.DatesView.as_view(), name='dates_form'),
+    re_path(r'form/places/(<int:iwork_id>)?', views.PlacesView.as_view(), name='places_form'),
+    re_path(r'form/manif/<int:iwork_id>', views.ManifView.as_view(), name='manif_init'),
+    re_path(r'form/manif/<int:iwork_id>/<str:manif_id>', views.ManifView.as_view(), name='manif_update'),
+    re_path(r'form/resources/(<int:iwork_id>)?', views.ResourcesView.as_view(), name='resources_form'),
+    re_path(r'form/details/(<int:iwork_id>)?', views.DetailsView.as_view(), name='details_form'),
+    path('form/overview/<int:iwork_id>', views.overview_view, name='overview_form'),
 ]
 urlpatterns.extend(
     url_utils.create_common_urls_for_section(
