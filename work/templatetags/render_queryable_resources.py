1e787e8bb7b339388863b0b3d0327e6cedacd11c
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2022-11-17 21:20:12 +0000

    Expanded search work results, fix render_queryable_resources.py

--- work/templatetags/render_queryable_resources.py
+++ work/templatetags/render_queryable_resources.py
@@ -11,13 +11,14 @@ link_pattern = re.compile(r'(xxxCofkLinkStartxxx)(xxxCofkHrefStartxxx)(.*?)(xxxC
 @register.filter
 def render_queryable_resources(values: str):
     resources = re.findall(link_pattern, values)
+    html = values
 
     if len(resources) > 1:
         html = '<ul>'
         for link in resources:
             html += f'<li><a href="{link[2]}">{link[4]}</a></li>'
         html += '</ul>'
-    else:
+    elif len(resources) == 1:
         html = f'<a href="{resources[0][2]}">{resources[0][4]}</a>'
 
     return mark_safe(html)
