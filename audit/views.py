8b79a4f554ef79a0bb5db5e4eb110a24218dd2d3
philip <philip.k@cottagelabs.com>
2022-12-02 13:14:07 +0000

    basic audit table  in search page

--- audit/views.py
+++ audit/views.py
@@ -1,8 +1,10 @@
+from typing import Callable, Iterable
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 
 from audit.models import CofkUnionAuditLiteral
-from core.helper.view_utils import BasicSearchView, DefaultSearchView
+from core.helper import renderer_utils
+from core.helper.view_utils import DefaultSearchView
 
 
 class AuditSearchView(LoginRequiredMixin, DefaultSearchView):
@@ -10,3 +12,7 @@ class AuditSearchView(LoginRequiredMixin, DefaultSearchView):
     def get_queryset(self):
         queryset = CofkUnionAuditLiteral.objects.all()
         return queryset
+
+    @property
+    def table_search_results_renderer_factory(self) -> Callable[[Iterable], Callable]:
+        return renderer_utils.create_table_search_results_renderer('audit/search_table_layout.html')
