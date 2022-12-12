213289c585e69e100504ccf30e7aca0477b01c8d
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2022-12-12 21:16:41 +0000

    Fix review display

--- uploader/templatetags/utils.py
+++ uploader/templatetags/utils.py
@@ -11,14 +11,24 @@ register = template.Library()
 log = logging.getLogger(__name__)
 
 
-@register.filter(name='filter_by_work')
+@register.filter
 def filter_by_work(queryset: QuerySet, work: CofkCollectWork):
     """Filters a queryset by work"""
     return queryset.filter(iwork=work).all()
 
 
-@register.filter(name='document_type')
+@register.filter
 def document_type(doc_type: str):
     """Looks document type description up"""
     return CofkLookupDocumentType.objects.\
         values_list('document_type_desc', flat=True).filter(document_type_code=doc_type).first()
+
+
+@register.simple_tag(takes_context=True)
+def get_location(context, loc_id: int):
+    loc = [loc for loc in context['places'] if loc.location_id == loc_id][0]
+
+    if loc.union_location:
+        return loc.union_location
+
+    return loc
