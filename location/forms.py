ec5b144a811f2631ea7ea91b251afd52797d4c06
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2022-12-20 11:47:31 +0000

    Use template for last changed by, add search fields to location

--- location/forms.py
+++ location/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm, HiddenInput, IntegerField, CharField
 
-from core.helper import form_utils
+from core.helper import form_utils, widgets_utils
 from location.models import CofkUnionLocation
 
 
@@ -81,3 +81,14 @@ class GeneralSearchFieldset(forms.Form):
 
     element_7_eg_empire = CharField(required=False, label='7. E.g. empire')
     element_7_eg_empire_lookup = form_utils.create_lookup_field(form_utils.StrLookupChoices.choices)
+
+    images = forms.CharField(required=False)
+    images_lookup = form_utils.create_lookup_field(form_utils.StrLookupChoices.choices)
+
+    change_timestamp_from = forms.CharField(required=False, widget=widgets_utils.NewDateInput())
+    change_timestamp_to = forms.CharField(required=False, widget=widgets_utils.NewDateInput())
+    change_timestamp_info = form_utils.datetime_search_info
+
+    change_user = forms.CharField(required=False, label='Last edited by',
+                                  help_text='Username of the person who last changed the record.')
+    change_user_lookup = form_utils.create_lookup_field(form_utils.StrLookupChoices.choices)
