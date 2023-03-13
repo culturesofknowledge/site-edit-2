667eb3207fb8ca3b2f93d5528fd99346a6306416
philip <philip.k@cottagelabs.com>
2023-03-13 12:35:41 +0000

    fixing download excel problem

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ def download_file(request, file_path):
     if not real_file_path.as_posix().startswith(media_service.FILE_DOWNLOAD_PATH.as_posix()):
         raise ValueError('Invalid path')
 
-    response = FileResponse(real_file_path.read_text(errors='ignore'),
+    response = FileResponse(real_file_path.open('rb'),
                             as_attachment=True,
                             filename=real_file_path.name)
     response['Content-Type'] = 'application/force-download'
