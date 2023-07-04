4a703dff23fddcfc60531386d07ad5f2fef5d1f6
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2023-07-04 17:57:15 +0100

    Add basic test to manifestation

--- manifestation/tests.py
+++ manifestation/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+from django.test import TransactionTestCase, RequestFactory
+
+from core.test.test_export_header_values import MockResolver
+from manifestation.views import ManifSearchView
+
+log = logging.getLogger(__name__)
+
+
+class ManifestationTestCase(TransactionTestCase):
+
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+
+    def test_no_results(self):
+        """
+        This test makes sure that a request to manifestation root URL returns
+        ane empty queryset.
+        """
+        request = self.factory.get("/manif")
+        request.resolver_match = MockResolver('manifestation')
+
+        view = ManifSearchView()
+        view.setup(request)
+
+        self.assertQuerysetEqual(view.get_queryset(), [])
