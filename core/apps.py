14cdb96d34d2847a3dc94ae5a6343b0af825e5a3
philip <philip.k@cottagelabs.com>
2024-05-13 10:04:22 +0100

    any other ways to run exporter schedule

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,27 @@
+import datetime
+
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from django_q.models import Schedule
+
+        from core.helper.exporter_serv import RUN_EXPORTER_FUNC
+        if not Schedule.objects.filter(func=RUN_EXPORTER_FUNC).count():
+            from django_q.tasks import schedule
+            schedule(
+                RUN_EXPORTER_FUNC,
+                schedule_type=Schedule.DAILY,
+                hours=0,
+
+                # schedule_type=Schedule.MINUTES,
+                # minutes=1,
+                q_options={'timeout': 24 * 60 * 60},
+
+                next_run=(datetime.datetime.now() + datetime.timedelta(days=1)).replace(hour=0, minute=0,
+                                                                                        second=0, microsecond=0),
+            )
--- core/helper/exporter_serv.py
+++ core/helper/exporter_serv.py
@@ -1,36 +1,48 @@
 import logging
 from pathlib import Path
 
-from django_q.models import Schedule
-from django_q.tasks import schedule
-
 from core import exporter
 from siteedit2.settings import EMLO_APP_HOME
 
 PATH_EXPORTER_OUTPUT = Path(EMLO_APP_HOME).joinpath('exporter')
+PATH_EXPORTER_OUTPUT.mkdir(parents=True, exist_ok=True)
+PATH_EXPORTER_FLAG = Path(EMLO_APP_HOME) / 'exporter_flag'
 
 log = logging.getLogger(__name__)
 
 RUN_EXPORTER_FUNC = 'core.helper.exporter_serv.run_exporter'
+ST_DONE = '0'
+ST_PENDING = '1'
+ST_RUNNING = '2'
 
 
 def is_exporter_pending() -> bool:
-    return Schedule.objects.filter(func=RUN_EXPORTER_FUNC).count()
+    return PATH_EXPORTER_FLAG.is_file() and PATH_EXPORTER_FLAG.read_text().strip() == ST_PENDING
 
 
 def mark_exporter_pending():
-    if not is_exporter_pending():
-        schedule(
-            RUN_EXPORTER_FUNC,
-            schedule_type=Schedule.DAILY,
-            hours=0,
-            # schedule_type=Schedule.MINUTES,
-            # minutes=1,
-            q_options={'timeout': 12 * 60 * 60, },
-        )
+    PATH_EXPORTER_FLAG.write_text(ST_PENDING)
+
+
+def mark_exporter_running():
+    PATH_EXPORTER_FLAG.write_text(ST_RUNNING)
+
+
+def mark_exporter_done():
+    PATH_EXPORTER_FLAG.write_text(ST_DONE)
 
 
 def run_exporter():
+    if not is_exporter_pending():
+        log.info('Exporter not pending')
+        return
+
+    mark_exporter_running()
     log.info('Exporter triggered')
-    exporter.export_all(output_dir=PATH_EXPORTER_OUTPUT, skip_url_check=False)
-    log.info('Exporter done')
+    try:
+        exporter.export_all(output_dir=PATH_EXPORTER_OUTPUT, skip_url_check=False)
+        log.info('Exporter done')
+    except Exception as e:
+        log.error('Exporter failed', exc_info=e)
+
+    mark_exporter_done()
