38e917bee6c593f4987c02a94a89eb938d25fb5c
philip <philip.k@cottagelabs.com>
2024-09-18 10:08:20 +0100

    add more extract data method

--- tombstone/features/feature_utils.py
+++ tombstone/features/feature_utils.py
@@ -16,21 +16,51 @@ def create_random_str(size=100, seed=42):
     return ''.join(random.choices(s, k=size))
 
 
-def get_str_or_random(record: dict | object, field_name, max_len=None):
-    default_str_len = max_len or 100
-    default_value = create_random_str(size=default_str_len)
+def get_attr_value(record, field_name, default_value=None):
     if isinstance(record, dict):
         val = record.get(field_name, default_value)
     else:
         val = getattr(record, field_name, default_value)
+    return val
+
+
+def get_str_or_random(record: dict | object, field_name, max_len=None):
+    default_str_len = max_len or 100
+    default_value = create_random_str(size=default_str_len)
+    val = get_attr_value(record, field_name, default_value)
     val = val or default_value
     if max_len:
         val = val[:max_len]
     return val
 
+
+def get_multi_str_or_random(record: dict | object, field_names: list[str], max_len=None):
+    default_str_len = max_len or 100
+    default_value = create_random_str(size=default_str_len)
+
+    vals = (get_attr_value(record, field_name, None)
+            for field_name in field_names)
+    vals = filter(None, vals)
+    vals = ' '.join(vals)
+    vals = vals or default_value
+    print(f'mixed_field: {vals}')
+    return vals
+
+
+def get_date_float_or_random(record, field_name):
+    default_val = random.randint(620000, 720000)
+    val = get_attr_value(record, field_name)
+    if val:
+        val = val.toordinal()
+    else:
+        val = default_val
+
+    return val
+
+
 def get_float_or_random(record, field_name, scale=1):
     default_value = random.random() * scale
-    val = getattr(record, field_name, default_value)
+    val = get_attr_value(record, field_name, default_value)
     if val is None:
         val = default_value
     try:
@@ -40,9 +70,9 @@ def get_float_or_random(record, field_name, scale=1):
     return val
 
 
-def create_text_pipeline(n_output_features=None):
+def create_text_pipeline(n_output_features=None, max_features=None):
     steps = [
-        ('tfidf', TfidfVectorizer()),
+        ('tfidf', TfidfVectorizer(max_features=max_features)),
     ]
     if n_output_features:
         steps.append(('pca', PCA(n_components=n_output_features)))
