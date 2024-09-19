f31077cdb253b7cf1ec92e1f8f249f20871d2f47
philip <philip.k@cottagelabs.com>
2024-09-19 13:34:17 +0100

    use extracted function, task_serv, FileBaseTaskStatusHandler

--- core/helper/exporter_serv.py
+++ core/helper/exporter_serv.py
@@ -1,48 +1,26 @@
+import functools
 import logging
 from pathlib import Path
 
+from cllib import inspect_utils
 from core import exporter
+from core.helper import task_serv
+from core.helper.task_serv import FileBaseTaskStatusHandler
 from siteedit2.settings import EMLO_APP_HOME
 
 PATH_EXPORTER_OUTPUT = Path(EMLO_APP_HOME).joinpath('exporter')
 PATH_EXPORTER_OUTPUT.mkdir(parents=True, exist_ok=True)
-PATH_EXPORTER_FLAG = Path(EMLO_APP_HOME) / 'exporter_flag'
 
 log = logging.getLogger(__name__)
 
-RUN_EXPORTER_FUNC = 'core.helper.exporter_serv.run_exporter'
-ST_DONE = '0'
-ST_PENDING = '1'
-ST_RUNNING = '2'
-
-
-def is_exporter_pending() -> bool:
-    return PATH_EXPORTER_FLAG.is_file() and PATH_EXPORTER_FLAG.read_text().strip() == ST_PENDING
-
-
-def mark_exporter_pending():
-    PATH_EXPORTER_FLAG.write_text(ST_PENDING)
-
-
-def mark_exporter_running():
-    PATH_EXPORTER_FLAG.write_text(ST_RUNNING)
-
-
-def mark_exporter_done():
-    PATH_EXPORTER_FLAG.write_text(ST_DONE)
+status_handler = FileBaseTaskStatusHandler(name='exporter_flag')
 
 
 def run_exporter():
-    if not is_exporter_pending():
-        log.info('Exporter not pending')
-        return
+    run_task_fn = functools.partial(exporter.export_all,
+                                    output_dir=PATH_EXPORTER_OUTPUT,
+                                    skip_url_check=False)
+    task_serv.run_task(run_task_fn, status_handler)
 
-    mark_exporter_running()
-    log.info('Exporter triggered')
-    try:
-        exporter.export_all(output_dir=PATH_EXPORTER_OUTPUT, skip_url_check=False)
-        log.info('Exporter done')
-    except Exception as e:
-        log.error('Exporter failed', exc_info=e)
 
-    mark_exporter_done()
+RUN_EXPORTER_FUNC = inspect_utils.get_fn_path(run_exporter)
