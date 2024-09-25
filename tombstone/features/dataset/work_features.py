3e612c472a732ec295633044eb0375c4cbe4bae4
philip <philip.k@cottagelabs.com>
2024-09-25 12:21:26 +0100

    change mixed_field

--- tombstone/features/dataset/work_features.py
+++ tombstone/features/dataset/work_features.py
@@ -21,14 +21,16 @@ REQUIRED_FIELDS = [
 ]
 
 FIELD_EXTRACTORS = {
-    'description': lambda r: feature_utils.get_str_or_random(r, 'description', 200),
-    'abstract': lambda r: feature_utils.get_str_or_random(r, 'abstract', 200),
-    'authors_as_marked': lambda r: feature_utils.get_str_or_random(r, 'authors_as_marked', 100),
-    'addressees_as_marked': lambda r: feature_utils.get_str_or_random(r, 'addressees_as_marked', 100),
-    'origin_as_marked': lambda r: feature_utils.get_str_or_random(r, 'origin_as_marked', 50),
-    'keywords': lambda r: feature_utils.get_str_or_random(r, 'keywords', 100),
-    'incipit': lambda r: feature_utils.get_str_or_random(r, 'incipit', 100),
-    'accession_code': lambda r: feature_utils.get_str_or_random(r, 'accession_code', 80),
+    'mixed_field': lambda r: feature_utils.get_multi_str_or_random(r, [
+        'description',
+        'abstract',
+        'authors_as_marked',
+        'addressees_as_marked',
+        'origin_as_marked',
+        'keywords',
+        'incipit',
+        'accession_code',
+    ]),
 }
 
 
@@ -40,13 +42,9 @@ def prepare_raw_df(records: Iterable[dict]) -> pd.DataFrame:
 def create_features(work_raw_df: pd.DataFrame):
     preprocessor = ColumnTransformer(
         transformers=[
-            ('description', feature_utils.create_text_pipeline(30), 'description'),
-            ('abstract', feature_utils.create_text_pipeline(15), 'abstract'),
-            ('authors_as_marked', feature_utils.create_text_pipeline(3), 'authors_as_marked'),
-            ('addressees_as_marked', feature_utils.create_text_pipeline(3), 'addressees_as_marked'),
-            ('keywords', feature_utils.create_text_pipeline(1), 'keywords'),
-            ('incipit', feature_utils.create_text_pipeline(1), 'incipit'),
-        ]
-    )
+            ('mixed_field', feature_utils.create_text_pipeline(
+                n_output_features=feature_utils.get_pca_n_components(100, work_raw_df.shape[0]),
+                max_features=10000), 'mixed_field'),
+        ])
     features = preprocessor.fit_transform(work_raw_df)
     return features
