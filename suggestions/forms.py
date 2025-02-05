6f26a88ec685037d3fc6307f5f63e37bae7d21b0
Anusha Ranganathan <anusha3@gmail.com>
2025-02-05 15:56:13 +0530

    Update to include filtering of displayed data

--- suggestions/forms.py
+++ suggestions/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import CofkSuggestions
 from core.helper import form_serv
 
+# Form to getting suggestion text
 class SuggestionForm(forms.Form):
     suggestion_text = forms.CharField(max_length = 4096, widget=forms.Textarea(attrs={'rows':30}))
 
@@ -9,20 +10,16 @@ class SuggestionForm(forms.Form):
         model = CofkSuggestions
         fields = ['suggestion_text']
 
+# Form for filtering displayed suggestions of the given user
 class SuggestionFilterForm(forms.Form):
-    # person       = forms.BooleanField(required=True, initial=True)
-    # location     = forms.BooleanField(required=True, initial=True)
-    # publication  = forms.BooleanField(required=True, initial=True)
-    # institution  = forms.BooleanField(required=True, initial=True)
-    # showNew      = forms.BooleanField(required=True, initial=True, label="Show New")
-    # showExisting = forms.BooleanField(required=True, initial=True, label="Show Existing")
-    person       = form_serv.ZeroOneCheckboxField(is_str=False, initial=1)
-    location     = form_serv.ZeroOneCheckboxField(is_str=False, initial=1)
-    publication  = form_serv.ZeroOneCheckboxField(is_str=False, initial=1)
-    institution  = form_serv.ZeroOneCheckboxField(is_str=False, initial=1)
-    showNew      = form_serv.ZeroOneCheckboxField(is_str=False, initial=1, label="Show New")
-    showExisting = form_serv.ZeroOneCheckboxField(is_str=False, initial=1, label="Show Existing")
+    person       = forms.BooleanField(required=True, initial=True, label="Person")
+    location     = forms.BooleanField(required=True, initial=True, label="Location")
+    publication  = forms.BooleanField(required=True, initial=True, label="Institution")
+    institution  = forms.BooleanField(required=True, initial=True, label="Publication")
+    showNew      = forms.BooleanField(required=True, initial=True, label="Show New")
+    showExisting = forms.BooleanField(required=True, initial=True, label="Show Existing")
 
     class Meta:
-        model = CofkSuggestions
-        fields = '__all__'
+        fields = [ 'person', 'location', 'publication', 'institution',
+                  'showNew', 'showExisting']
+    
