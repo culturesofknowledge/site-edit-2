cab895510cce69f4b37642b06f4aea5a862d0cb9
Ramakrishna Sakhamuru <rama@cottagelabs.com>
2025-11-29 20:55:23 +0530

    Publication help text

--- publication/forms.py
+++ publication/forms.py
@@ -8,8 +8,15 @@ from publication.models import CofkUnionPublication
 
 
 class PublicationForm(ModelForm):
-    publication_details = form_serv.CommonTextareaField(required=True)
-    abbrev = forms.CharField(required=False, max_length=50)
+    publication_details = form_serv.CommonTextareaField(
+        required=True,
+        help_text='E.g. author(s)/editor(s), title, place and year of publication.'
+    )
+    abbrev = forms.CharField(
+        required=False,
+        max_length=50,
+        help_text='Optional short form of the full publication details.'
+    )
 
     class Meta:
         model = CofkUnionPublication
@@ -23,11 +30,19 @@ class GeneralSearchFieldset(form_serv.BasicSearchFieldset):
     title = 'General'
     template_name = 'publication/component/publication_search_fieldset.html'
 
-    publication_details = SearchCharField()
+    publication_details = SearchCharField(
+        help_text='E.g. author(s)/editor(s), title, place and year of publication.'
+    )
     publication_details_lookup = form_serv.create_lookup_field(form_serv.StrLookupChoices.choices)
 
-    abbrev = SearchCharField(label=field_label_map['publication']['abbrev'])
+    abbrev = SearchCharField(
+        label=field_label_map['publication']['abbrev'],
+        help_text='Optional short form of the full publication details.'
+    )
     abbrev_lookup = form_serv.create_lookup_field(form_serv.StrLookupChoices.choices)
 
-    publication_id = SearchIntField(label=field_label_map['publication']['publication_id'])
+    publication_id = SearchIntField(
+        label=field_label_map['publication']['publication_id'],
+        help_text='The unique ID for the record within this database.'
+    )
     publication_id_lookup = form_serv.create_lookup_field(form_serv.IntLookupChoices.choices)
