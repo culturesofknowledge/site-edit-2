8c28759cc85cbae5e9fa635fbd95507df344a3b0
Ramakrishna Sakhamuru <rama@cottagelabs.com>
2026-01-27 10:24:17 +0530

    Add permissions setup to entrypoint script

--- docker/site-edit-2/entrypoint.sh
+++ docker/site-edit-2/entrypoint.sh
@@ -7,4 +7,7 @@ python manage.py migrate
 # Create cache table
 echo "Creating cache table"
 python manage.py createcachetable
+echo "add/update permissions"
+python manage.py add_groups_and_permissions
+echo "Starting server ..."
 python -u manage.py runserver 0.0.0.0:8000
