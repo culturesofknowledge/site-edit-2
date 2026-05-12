5b0712e033dda27e316786d1490688b839d57774
Anusha Ranganathan <anusha@cottagelabs.com>
2026-05-12 23:29:14 +0530

    Fix overview tab display

--- work/views.py
+++ work/views.py
@@ -884,10 +884,16 @@ def to_person_link_list(work, rel_type):
     return (PersonLinkData(r.person) for r in
             work.cofkworkpersonmap_set.filter(relationship_type=rel_type))
 
+def to_person_link_count(work, rel_type):
+    return len(work.cofkworkpersonmap_set.filter(relationship_type=rel_type))
+
 
 def to_location_link_list(work, rel_type):
     return (LocationLinkData(r.location) for r in
             work.cofkworklocationmap_set.filter(relationship_type=rel_type))
+
+def to_location_link_count(work, rel_type):
+    return len(work.cofkworklocationmap_set.filter(relationship_type=rel_type))
 
 
 def to_overview_manif(manif: CofkUnionManifestation):
@@ -921,33 +927,48 @@ def overview_view(request, iwork_id):
         notes_general=work_serv.find_related_comment_names(work, REL_TYPE_COMMENT_REFERS_TO),
 
         author_link_list=to_person_link_list(work, constant.REL_TYPE_CREATED),
+        author_link_count=to_person_link_count(work, constant.REL_TYPE_CREATED),
         sender_link_list=to_person_link_list(work, constant.REL_TYPE_SENT),
+        sender_link_count=to_person_link_count(work, constant.REL_TYPE_SENT),
         signed_link_list=to_person_link_list(work, constant.REL_TYPE_SIGNED),
+        signed_link_count=to_person_link_count(work, constant.REL_TYPE_SIGNED),
 
         recipient_link_list=to_person_link_list(work, constant.REL_TYPE_WAS_ADDRESSED_TO),
+        recipient_link_count=to_person_link_count(work, constant.REL_TYPE_WAS_ADDRESSED_TO),
         intended_link_list=to_person_link_list(work, constant.REL_TYPE_INTENDED_FOR),
+        intended_link_count=to_person_link_count(work, constant.REL_TYPE_INTENDED_FOR),
 
         reply_to_link_list=[WorkLinkData(r.work_to) for r in
                             work.work_from_set.filter(relationship_type=constant.REL_TYPE_WORK_IS_REPLY_TO)],
+        reply_to_link_count=len(work.work_from_set.filter(relationship_type=constant.REL_TYPE_WORK_IS_REPLY_TO)),
         answered_link_list=[WorkLinkData(r.work_from) for r in
                             work.work_to_set.filter(relationship_type=constant.REL_TYPE_WORK_IS_REPLY_TO)],
+        answered_link_count=len(work.work_to_set.filter(relationship_type=constant.REL_TYPE_WORK_IS_REPLY_TO)),
         matches_link_list=[WorkLinkData(r.work_to) for r in
                            work.work_from_set.filter(relationship_type=constant.REL_TYPE_WORK_MATCHES)]
                          + [WorkLinkData(r.work_from) for r in
                             work.work_to_set.filter(relationship_type=constant.REL_TYPE_WORK_MATCHES)],
+        matches_link_count=len(work.work_from_set.filter(relationship_type=constant.REL_TYPE_WORK_MATCHES))
+                          + len(work.work_to_set.filter(relationship_type=constant.REL_TYPE_WORK_MATCHES)),
 
         origin_link_list=to_location_link_list(work, constant.REL_TYPE_WAS_SENT_FROM),
+        origin_link_count=to_location_link_count(work, constant.REL_TYPE_WAS_SENT_FROM),
         destination_link_list=to_location_link_list(work, constant.REL_TYPE_WAS_SENT_TO),
+        destination_link_count=to_location_link_count(work, constant.REL_TYPE_WAS_SENT_TO),
 
         language=', '.join(map(_to_lang_str, work.language_set.iterator())),
         subjects=', '.join(w.subject.subject_desc for w in work.cofkworksubjectmap_set.iterator()),
 
         people_link_list=to_person_link_list(work, constant.REL_TYPE_MENTION),
+        people_link_count=to_person_link_count(work, constant.REL_TYPE_MENTION),
         places_link_list=to_location_link_list(work, constant.REL_TYPE_MENTION_PLACE),
+        places_link_count=to_location_link_count(work, constant.REL_TYPE_MENTION_PLACE),
         work_mention_link_list=(WorkLinkData(r.work_to) for r in
                                 work.work_from_set.filter(relationship_type=constant.REL_TYPE_MENTION_WORK)),
+        work_mention_link_count=len(work.work_from_set.filter(relationship_type=constant.REL_TYPE_MENTION_WORK)),
         work_be_mention_link_list=(WorkLinkData(r.work_from) for r in
                                    work.work_to_set.filter(relationship_type=constant.REL_TYPE_MENTION_WORK)),
+        work_be_mention_link_count=len(work.work_to_set.filter(relationship_type=constant.REL_TYPE_MENTION_WORK)),
         manif_set=list(map(to_overview_manif, work.manif_set.iterator())),
         original_calendar_display=date_serv.decode_calendar(work.original_calendar),
         work_category='Overview'
