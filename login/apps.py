b2f705ad4168a37715c3fed42783e9546965a1aa
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2026-09-14 14:47:27 +0100

    Implement failed logins

--- login/apps.py
+++ login/apps.py
@@ -6,7 +6,8 @@ class LoginConfig(AppConfig):
     name = 'login'
 
     def ready(self):
-        from django.contrib.auth.signals import user_logged_in
+        from django.contrib.auth.signals import user_logged_in, user_login_failed
+        from django.db.models import F
         from django.utils import timezone
         from login.models import CofkUser
 
@@ -15,4 +16,17 @@ class LoginConfig(AppConfig):
             user.login_time = timezone.now()
             user.save(update_fields=['prev_login', 'login_time'])
 
+        def increment_failed_logins(sender, credentials, request=None, **kwargs):
+            # credentials is sanitised (password redacted) but the username
+            # field is left as-is. No user instance is available here --
+            # the username may not even match a real account -- so update()
+            # rather than fetch+save, which is a no-op for an unknown user
+            # instead of raising DoesNotExist.
+            username = credentials.get('username')
+            if username:
+                CofkUser.objects.filter(pk=username).update(failed_logins=F('failed_logins') + 1)
+
         user_logged_in.connect(update_login_times, sender=CofkUser, weak=False)
+        # user_login_failed is sent with sender=<module name>, not the user
+        # model, so it can't be filtered by sender the way user_logged_in is.
+        user_login_failed.connect(increment_failed_logins, weak=False)
